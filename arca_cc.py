99d1eaaaeaaebaf57a9ca04b8d006cb07c430984
SATORIT <67799965+satoritter5@users.noreply.github.com>
2021-02-08 04:13:12 +0900

    작동패치
    아카라이브 업데이트로 인한 레이아웃 변경으로 다시 작업했습니다.
    그리고 티는 안나지만 readme도 조금 손봄

--- arca_cc.py
+++ arca_cc.py
@@ -20,9 +20,6 @@ end_page = 끝페이지(숫자만)
 ####################
 
 
-
-
-
 #webdriver options
 options = webdriver.ChromeOptions()
 options.add_argument("disable-gpu")
@@ -34,7 +31,6 @@ driver = webdriver.Chrome(wd_locate, options=options)
 login_url = 'https://arca.live/u/login?goto=/b/' + board
 arc_url = 'https://arca.live/b/' + board + '?' + 'category=' + category + '&target=all&keyword=&p='
 
-
 #아카라이브 로그인
 print('로그인을 시도합니다.')
 driver.get(login_url)
@@ -43,12 +39,10 @@ driver.find_element_by_xpath('//*[@id="idInput"]').send_keys(login_id)
 driver.find_element_by_xpath('//*[@id="idPassword"]').send_keys(login_password)
 driver.find_element_by_xpath('/html/body/div/div[3]/article/div/div[2]/div/div/form/div[3]/button').click()
 
-
 #아카라이브 채널 열기
 print('채널에 접속합니다.')
 driver.get(arc_url)
 time.sleep(1)
-
 
 #경고창 스킵
 try:
@@ -57,15 +51,15 @@ try:
 except:
     pass
 
-
 #국가제한 변경구문
 for page in range(start_page, end_page):
     url = arc_url + str(page)
-    time.sleep(1)
     driver.get(url)
+    time.sleep(1)
     #게시글은 2부터 시작하고, 한페이지에 표기되는 게시글 수는 총 20개 입니다.
     for op in range(2, 22):
         print('[ 현재 게시글은 '+str(page)+' 페이지의 '+str(op)+' 번째 게시글 입니다. ]\n')
+        
         #새창으로 게시글 열기
         opget = driver.find_element_by_xpath('/html/body/div/div[3]/article/div/div[4]/div[3]/a['+str(op)+']')
         opget.send_keys(Keys.CONTROL + Keys.ENTER)
@@ -77,9 +71,9 @@ for page in range(start_page, end_page):
         
         #국가제한 클릭
         try:
-            driver.find_element_by_xpath('/html/body/div/div[3]/article/div[2]/div[3]/div[1]/span[3]/span[2]/a[4]').click()
+            driver.find_element_by_xpath('/html/body/div/div[3]/article/div[2]/div[3]/div[1]/span[3]/span[2]/a[6]').click()
         except:
-            driver.find_element_by_xpath('/html/body/div/div[3]/article/div[2]/div[2]/div[1]/span[3]/span[2]/a[4]').click()
+            driver.find_element_by_xpath('/html/body/div/div[3]/article/div[2]/div[2]/div[1]/span[3]/span[2]/a[6]').click()
         time.sleep(0.1)
         for check in ban_country:
             if driver.find_element_by_xpath('/html/body/div[1]/div[3]/article/div[1]/div/form/div[2]/div/div/div['+str(check)+']/input').get_attribute('checked'):
@@ -89,7 +83,7 @@ for page in range(start_page, end_page):
                 driver.find_element_by_xpath('/html/body/div[1]/div[3]/article/div[1]/div/form/div[2]/div/div/div['+str(check)+']/input').click()
                 print('이 게시글의 지역설정을 완료했습니다.\n\n')
                 driver.find_element_by_xpath('//*[@id="modal-country-restriction"]/div/form/div[3]/button[2]').click()
-        time.sleep(0.2)
+        time.sleep(0.1)
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
     print('\n\n※[ 다음 페이지로 넘어갑니다. ]※\n\n\n\n')
